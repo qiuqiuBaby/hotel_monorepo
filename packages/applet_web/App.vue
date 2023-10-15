e4391cd492632d0a6a3ca5ac8a6984f1cd776ff8
jing.ouyang <jing.ouyang@lkcoffee.com>
2023-10-15 22:40:16 +0800

    feat: monorepo初始化

---
+++ packages/applet_web/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { isNumber } from '@hotel/utils'
+import { CBtn } from '@hotel/components'
+import { fetchUsersList } from '@hotel/apis'
+console.log(isNumber('11sda'), 'isNumber========');
+const res = fetchUsersList()
+console.log('res========', res);
+
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
